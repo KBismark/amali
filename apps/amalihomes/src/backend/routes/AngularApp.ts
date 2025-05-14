189f7a73084673cd1f4433e6376a2aa1c1c31fa6
KBismark <bismarkkwabenayamoah@gmail.com>
2025-05-14 14:35:55 +0000

    Remove debug logging from Angular application request handling

--- apps/amalihomes/src/backend/routes/AngularApp.ts
+++ apps/amalihomes/src/backend/routes/AngularApp.ts
@@ -4,18 +4,10 @@ import express from 'express';
 export const angularAppRouter = express.Router();
 
 const angularApp = new AngularNodeAppEngine();
-
-angularAppRouter.use((req, res, next)=>{
-  console.log('Reached here: ', req.baseUrl, req.path, req.originalUrl);
-  next();
-});
-
 /**
  * Handle all other requests by rendering the Angular application.
  */
 angularAppRouter.use('*', (req, res, next) => {
-  console.log('Reached here too: ',  req.baseUrl, req.path, req.originalUrl);
-
   angularApp
     .handle(req)
     .then((response) => (response ? writeResponseToNodeResponse(response, res) : next()))
